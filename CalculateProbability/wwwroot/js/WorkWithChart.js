06e113e54855778a0212c3241a3d974cff78e7a0
GMIKE <roman.soloweow333@ya.ru>
2019-11-27 02:35:12 +0300

    Add Chart

---
+++ CalculateProbability/wwwroot/js/WorkWithChart.js
@@ -0,0 +1,79 @@
+﻿function resetZoom() {
+    window.myLine.resetZoom();
+}
+
+let config = {
+    type: 'line',
+    datasets: [{
+    }],
+
+    options: {
+        legend: {
+            display: true,
+            position: 'top',
+            labels: {
+                boxWidth: 80,
+                fontColor: 'black'
+            }
+        },
+        scales: {
+            xAxes: [
+                {
+                    scaleLabel: {
+                        display: true
+                    }
+                }
+            ],
+            yAxes: [
+                {
+                    scaleLabel: {
+                        display: true,
+                        labelString: "Значение"
+                    }
+                }
+            ]
+        },
+        pan: {
+            enabled: true,
+            mode: 'xy',
+            speed: 1
+        },
+        zoom: {
+            enabled: true,
+            drag: false,
+            mode: 'xy',
+            speed: 0.01
+        }
+    }
+
+};
+
+window.onload = function () {
+    var ctx = document.getElementById("canvas");
+    window.myLine = new Chart(ctx, config);
+};
+
+function ShowChart()
+{
+    color = "27,110,194"
+    dataArr =
+     [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    ]
+    config.data = {
+        labels: dataArr[0],
+        datasets: [{
+            label: "График",
+            data: dataArr[1],
+            borderColor: "rgb( "+color+")",
+            backgroundColor: "rgb( " + color + ",0.5)",
+            pointBorderColor: "rgb( " + color + ")",
+            pointBackgroundColor: "rgb( " + color + ")",
+            pointBorderWidth: 1,
+            fill: true
+        }]
+    }
+    config.options.scales.xAxes[0].scaleLabel.labelString = "new Data"
+    window.myLine.update();
+}
