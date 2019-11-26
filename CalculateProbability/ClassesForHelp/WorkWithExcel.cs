9fa64f977ce6faaa375386d5b3c4de2e69c0f49d
GMIKE <roman.soloweow333@ya.ru>
2019-11-27 01:40:43 +0300

    Start learn exceldatareader

---
+++ CalculateProbability/ClassesForHelp/WorkWithExcel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalculateProbability
+{
+    public static class WorkWithExcel
+    {
+        public static void Open()
+        {
+            //using (var stream = File.Open(""))
+            //{
+
+            //}
+        }
+       
+    }
+}
