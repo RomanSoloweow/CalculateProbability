fbe66d3ed579bf5215c133548534e17b5478b95c
GMIKE <roman.soloweow333@ya.ru>
2019-11-27 23:27:12 +0300

    Test data

--- CalculateProbability/Pages/Index.cshtml.cs
+++ CalculateProbability/Pages/Index.cshtml.cs
@@ -13,7 +13,7 @@ namespace CalculateProbability.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
+        private int[,] data = new int[3, 2] { { 1, 3 }, { 6, 2 }, { 8, 5 } };
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -25,8 +25,6 @@ namespace CalculateProbability.Pages
         }
         public ActionResult OnPostGetData(string ParametrName,double From, double To, double CountDote, double Tn,double T0, double S, double F, double Fv, double Eps )
         {
-
-            int[,] data = new int[3, 2] { { 1, 3 }, { 6, 2 }, { 8, 5 } };
             var result = JsonConvert.SerializeObject(data);
             return new JsonResult(result);
         }
