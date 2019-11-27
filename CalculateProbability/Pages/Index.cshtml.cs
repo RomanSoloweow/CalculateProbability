ca2f9145b0b2eb8763e56473aa120793d6af773a
GMIKE <roman.soloweow333@ya.ru>
2019-11-27 21:34:15 +0300

    Add Template for Post Queries

--- CalculateProbability/Pages/Index.cshtml.cs
+++ CalculateProbability/Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace CalculateProbability.Pages
 {
---
+++ CalculateProbability/wwwroot/js/PostQueries.js
@@ -0,0 +1,23 @@
+﻿function Post(Handler, Data, FunctionOnSuccess, FunctionOnFailure)
+{
+    $.ajax({
+        type: "Post",
+        url: "?handler=" + Handler,
+        data: Data,
+        headers:
+        {
+            "RequestVerificationToken": $('input:hidden[name="__RequestVerificationToken"]').val()
+        },
+        dataType: "json",
+        success: function (response)
+        {
+            FunctionOnSuccess(response);
+        },
+        failure: function (response)
+        {
+            if (FunctionOnFailure)
+                FunctionOnFailure(response);
+        }
+    });
+
+}
